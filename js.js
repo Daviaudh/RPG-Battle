c97b8cb5720e0ea2eccff73f153e99aca4566084
hdaviaud <daviaudh1@gmail.com>
2020-12-07 19:21:16 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,15 +1,15 @@
-document.getElementById('monstre2').onmouseover= function(){
-	document.getElementById('infobulleMonstre2').style.visibility='visible'
-}
-document.getElementById('monstre2').onmouseout= function(){
-	document.getElementById('infobulleMonstre2').style.visibility='hidden'
-}
-
 document.getElementById('monstre1').onmouseover= function(){
 	document.getElementById('infobulleMonstre1').style.visibility='visible'
 }
 document.getElementById('monstre1').onmouseout= function(){
 	document.getElementById('infobulleMonstre1').style.visibility='hidden'
+}
+
+document.getElementById('monstre2').onmouseover= function(){
+	document.getElementById('infobulleMonstre2').style.visibility='visible'
+}
+document.getElementById('monstre2').onmouseout= function(){
+	document.getElementById('infobulleMonstre2').style.visibility='hidden'
 }
 
 document.getElementById('monstre3').onmouseover= function(){
