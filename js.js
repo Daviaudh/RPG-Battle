15ca35fd3a28056c0d22fceeb53cecb14a89b214
hdaviaud <daviaudh1@gmail.com>
2020-12-07 19:20:46 +0100

    infoBulleHidden

---
+++ js.js
@@ -0,0 +1,20 @@
+document.getElementById('monstre2').onmouseover= function(){
+	document.getElementById('infobulleMonstre2').style.visibility='visible'
+}
+document.getElementById('monstre2').onmouseout= function(){
+	document.getElementById('infobulleMonstre2').style.visibility='hidden'
+}
+
+document.getElementById('monstre1').onmouseover= function(){
+	document.getElementById('infobulleMonstre1').style.visibility='visible'
+}
+document.getElementById('monstre1').onmouseout= function(){
+	document.getElementById('infobulleMonstre1').style.visibility='hidden'
+}
+
+document.getElementById('monstre3').onmouseover= function(){
+	document.getElementById('infobulleMonstre3').style.visibility='visible'
+}
+document.getElementById('monstre3').onmouseout= function(){
+	document.getElementById('infobulleMonstre3').style.visibility='hidden'
+}
